d8dcebc2b18e1c4161b73c63edc6694806d8cea4
Aryan Shah <aryanwork10@gmail.com>
2024-11-21 20:39:59 +0530

    bugs fixed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -48,6 +48,7 @@ const Login = () => {
         headers: {
           'Content-Type': 'application/json'
         },
+        credentials: 'include',
         body: JSON.stringify({email, password})
       });
 
